f17414d3b99f0fcbc41c330f43f96a288b802851
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-11-11 15:49:43 +0100

    Liskov Substitution Principle demonstration.

---
+++ src/main/java/Solid/LiskovSubstitutionPrinciple/RectangleFactory.java
@@ -0,0 +1,12 @@
+package Solid.LiskovSubstitutionPrinciple;
+
+public class RectangleFactory {
+
+    public static Rectangle newSquare(int side) {
+        return new Rectangle(side, side);
+    }
+
+    public static Rectangle newRectangle(int width, int height) {
+        return new Rectangle(width, height);
+    }
+}
